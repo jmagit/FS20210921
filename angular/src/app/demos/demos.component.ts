d0510b89aba3b782d6198c0fdb65c6d15fbbfb8f
Javier <jmartin@grupoloyal.com>
2021-10-05 10:37:23 +0200

    clase del componente

--- angular/src/app/demos/demos.component.ts
+++ angular/src/app/demos/demos.component.ts
@@ -6,8 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demos.component.scss']
 })
 export class DemosComponent implements OnInit {
+  private nombre: string = 'mundo';
+  listado = [
+    { id: 1, nombre: 'Madrid' },
+    { id: 2, nombre: 'malaga' },
+    { id: 3, nombre: 'SEVILLA' },
+    { id: 4, nombre: 'ciudad real' },
+  ]
+  idProvincia: number = 2;
+
+  resultado: string | null = null;
+  visible = true;
+  estetica = { importante: true, error: false, urgente: true };
+  fontSize = 14;
 
   constructor() { }
+
+  public get Nombre(): string { return this.nombre; }
+  public set Nombre(value: string) {
+    if (this.nombre === value) return;
+    this.nombre = value;
+  }
+
+  public saluda(): void {
+    this.resultado = `Hola ${this.nombre}`;
+  }
+
+  despide(): void {
+    this.resultado = `Adios ${this.nombre}`;
+  }
+
+  di(algo: string): void {
+    this.resultado = `Dice ${algo}`;
+  }
+
+  cambia(): void {
+    this.visible = !this.visible;
+    this.estetica.importante = !this.estetica.importante;
+    this.estetica.error = !this.estetica.error;
+  }
+
+  calcula(a: number, b: number): number { return a + b; }
+
+  add(provincia: string): void {
+    const id = this.listado.length === 0 ? 1 : (this.listado[this.listado.length - 1].id + 1);
+    this.listado.push({ id, nombre: provincia });
+    this.idProvincia = id;
+  }
 
   ngOnInit(): void {
   }
