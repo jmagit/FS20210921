f817788562108692125e9d2fd3c9d4950d22625e
Javier <jmartin@grupoloyal.com>
2021-10-07 13:46:37 +0200

    validaciones

--- angular/src/app/formulario/formulario.component.ts
+++ angular/src/app/formulario/formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
+import { NotificationService, NotificationType } from '../common-services';
 
 export interface Persona {
   id: number | null;
@@ -11,9 +12,47 @@ export interface Persona {
 }
 
 @Injectable({providedIn: 'root'})
-export class ClientesViewModel {
-  constructor() { }
+export class PersonasViewModel {
+  Listado: Array<Persona> = [
+    { id: 1, nombre: 'Pepito', apellidos: 'Grillo', correo: 'pepito@grillo', edad: 99, dni: '12345678Z' }
+  ]
+  Elemento: Persona = { id: null, nombre: '', apellidos: '', correo: null, edad: null, dni: null };
+  IsAdd = true;
 
+  constructor(private notify: NotificationService) {
+
+  }
+
+  public list() {
+
+  }
+
+  public add() {
+    this.Elemento = { id: null, nombre: '', apellidos: '', correo: null, edad: null, dni: null }
+    this.IsAdd = true;
+  }
+
+  public edit() {
+    this.Elemento = this.Listado[0];
+    this.IsAdd = false;
+  }
+
+  public view() {
+    this.Elemento = this.Listado[0];
+    this.IsAdd = false;
+  }
+
+  public delete() {
+
+  }
+
+  public cancel() {
+
+  }
+
+  public send() {
+    this.notify.add((this.IsAdd ? 'Nuevos: ' : 'Modificados: ') + JSON.stringify(this.Elemento), NotificationType.info);
+  }
 }
 
 @Component({
@@ -23,7 +62,7 @@ export class ClientesViewModel {
 })
 export class FormularioComponent implements OnInit {
 
-  constructor(public vm: ClientesViewModel) { }
+  constructor(public vm: PersonasViewModel) { }
 
   ngOnInit(): void {
   }
