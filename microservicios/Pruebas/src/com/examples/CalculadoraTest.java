847324ecea7c609ff457e3c999760c9b6796487a
Javier <jamartiton@gmail.com>
2021-10-18 18:58:16 +0200

    Ejemplos

--- microservicios/Pruebas/src/com/examples/CalculadoraTest.java
+++ microservicios/Pruebas/src/com/examples/CalculadoraTest.java
@@ -18,6 +18,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+// Ejemplos 
+
 class CalculadoraTest {
 	Calculadora calc;
 
