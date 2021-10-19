eb1d0c73cca7b11badcc959bf0538fb29d4d6571
Javier <jamartiton@gmail.com>
2021-10-19 18:06:57 +0200

    Mock

--- microservicios/Pruebas/src/com/examples/CalculadoraTest.java
+++ microservicios/Pruebas/src/com/examples/CalculadoraTest.java
@@ -2,6 +2,7 @@ package com.examples;
 
 import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -15,8 +16,11 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 class CalculadoraTest {
 	Calculadora calc;
@@ -59,6 +63,13 @@ class CalculadoraTest {
 	@Nested
 	@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 	class Divisiones {
+		Calculadora calc;
+
+		@BeforeEach
+		void setUp() throws Exception {
+			calc = new Calculadora();
+		}
+		
 		@RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
 		void test_Divide_Double_Double() {
 //			assertEquals(0.5, calc.divide(1.0, 2));
@@ -86,4 +97,19 @@ class CalculadoraTest {
 			assertThrows(Exception.class, () -> calc.divide(1, 0));
 		}
 	}
+	
+//	@Nested
+//	@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+//	@ExtendWith(MockitoExtension.class)
+//	class Mockeado {
+//		@Mock
+//		Calculadora calculadora;
+//		
+//		@Test
+//		void suma_mock() {
+//			when(calculadora.suma(2, 2)).thenReturn(2.0);
+//			
+//			assertEquals(calculadora.suma(2, 2), 2);
+//		}
+//	}
 }
