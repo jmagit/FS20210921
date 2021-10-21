2021d82e759b6d5e0cc02aa2333a9edacd5a7540
Javier <jamartiton@gmail.com>
2021-10-21 11:56:05 +0200

    DomainService

---
+++ microservicios/demo/src/main/java/com/example/domains/core/services/contracts/DomainService.java
@@ -0,0 +1,13 @@
+package com.example.domains.core.services.contracts;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DomainService<E, K> {
+	List<E> getAll();
+	Optional<E> getOne(K id);
+	E add(E item);
+	E modify(E item);
+	void delete(E item);
+	void deleteById(K id);
+}
