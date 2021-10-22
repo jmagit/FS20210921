ed2a6e64ecc3ecfdd6dadd0ef3ae07b32de16d0d
Javier <jamartiton@gmail.com>
2021-10-22 17:15:17 +0200

    rest

--- microservicios/demo/src/main/java/com/example/application/resource/DemosResource.java
+++ microservicios/demo/src/main/java/com/example/application/resource/DemosResource.java
@@ -1,6 +1,5 @@
 package com.example.application.resource;
 
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,7 +11,7 @@ public class DemosResource {
 	@GetMapping("/params/{id}")
 	public String cotilla(
 	        @PathVariable String id,
-	        @RequestParam String nom,
+	        @RequestParam(required = false, defaultValue = "valor por defecto") String nom,
 	        @RequestHeader("Accept-Language") String language) { 
 	    StringBuilder sb = new StringBuilder();
 	    sb.append("id: " + id + "\n");
